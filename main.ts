713f4a3a087d87f38d9550af85bf88ef46449cdd
Sarah Townson <26084137+Isioviel@users.noreply.github.com>
2022-03-11 17:46:37 +0000

    Update main.blocks, main.ts, robotBuggy.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,14 @@
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < 4; index++) {
-        robotBuggy.movement(Dir.forward, Speed.medium, 2000)
-        robotBuggy.movement(Dir.left, Speed.fast, 1000)
-        robotBuggy.movement(Dir.backward, Speed.medium, 1000)
-    }
+    basic.showIcon(IconNames.Heart)
+    robotBuggy.movement(Dir.forward, Speed.medium, 2000)
+    basic.clearScreen()
 })
-robotBuggy.setup(
-Pin.p13,
-Pin.p12,
-Pin.p15,
-Pin.p14
-)
+input.onButtonPressed(Button.B, function () {
+    robotBuggy.movementAdvanced(
+    Motors.both,
+    750,
+    2000,
+    IconNames.Heart
+    )
+})
+robotBuggy.activateRobot()
--- robotBuggy.ts
+++ robotBuggy.ts
@@ -29,13 +29,33 @@ enum Pin {
     p16 = 116
 }
 
+enum Motors {
+    left,
+    right,
+    //% block="left+right"
+    both
+}
+
 let LF = 113;
 let LB = 112;
 let RF = 115;
 let RB = 114;
 
+//% weight=100 color=#CCCC00 icon="U+263C"
 namespace robotBuggy {
     
+    /**
+     * Stop your robot's motors
+     */
+    //% block="stop robot motors"
+    //% advanced=true
+    export function stopRobot() {
+        pins.analogWritePin(LF, 0)
+        pins.analogWritePin(LB, 0)
+        pins.analogWritePin(RF, 0)
+        pins.analogWritePin(RB, 0)
+    }
+
     /**
      * Set up which pins the motor driver is connected to
      * @param lf sets pin value for left motor forwards e.g. Pin.p13
@@ -43,22 +63,32 @@ namespace robotBuggy {
      * @param rf sets pin value for right motor forwards e.g. Pin.p15
      * @param rb sets pin value for right motor reverse e.g. Pin.p14
      */
-    //% block="set up LF %lf set up LB %lb set up RF %rf set up RB %rb"
+    //% block="set up A1A %lf set up A1B %lb set up B1A %rf set up B1B %rb"
     //% lf.defl=Pin.p13 lb.defl=Pin.p12 rf.defl=Pin.p15 rb.defl=Pin.p14
+    //% advanced=true
     export function setup(lf: Pin, lb: Pin, rf: Pin, rb: Pin) {
         LF = lf,
         LB = lb,
         RF = rf,
         RB = rb
+        stopRobot()
     }
 
+    /**
+     * Sets up your robot to the default settings
+     */
+    //% block="activate robot"
+    export function activateRobot() {
+        stopRobot()
+    }
+    
     /**
      * Move your robot for a set amount of time
      * @param direction choose a direction, eg: forward
      * @param value choose a speed, eg: medium
      * @param duration choose how long the motor runs for in ms, eg: 1000
      */
-    //% block="set direction %direction at speed %value for duration: %duration"
+    //% block="set direction %direction at speed %value for duration %duration"
     //% value.defl=medium
     //% duration.shadow=timePicker
     export function movement(direction: Dir, value: Speed, duration: number) {
@@ -75,5 +105,41 @@ namespace robotBuggy {
             pins.analogWritePin(LF, Math.abs(value))
             pins.analogWritePin(RB, Math.abs(value))
         }
+        basic.pause(duration)
+        stopRobot()
     }
+
+    /**
+     * Control your robot's motors
+     * @param motor choose a motor, eg: left
+     * @param value choose a speed, eg: 750
+     * @param duration choose how long the motor runs for in ms, eg: 1000
+     * @param image choose which icon to display whilst moving, eg: duck
+     */
+    //% block="set motor %motor at speed %value for duration %duration with image %image"
+    //% value.min=-1023 value.max=1023 value.defl=750
+    //% duration.shadow=timePicker
+    //% advanced=true
+    export function movementAdvanced(motor: Motors, value: number, duration: number, image: IconNames) {
+        images.iconImage(image).showImage(0)
+        if (motor == Motors.left && value > 0) {
+            pins.analogWritePin(LF, Math.abs(value))
+        } else if (motor == Motors.left && value < 0) {
+            pins.analogWritePin(LB, Math.abs(value))
+        } else if (motor == Motors.right && value > 0) {
+            pins.analogWritePin(RF, Math.abs(value))
+        } else if (motor == Motors.right && value < 0) {
+            pins.analogWritePin(RB, Math.abs(value))
+        } else if (motor == Motors.both && value > 0) {
+            pins.analogWritePin(LF, Math.abs(value))
+            pins.analogWritePin(RF, Math.abs(value))
+        } else if (motor == Motors.both && value < 0) {
+            pins.analogWritePin(LB, Math.abs(value))
+            pins.analogWritePin(RB, Math.abs(value))
+        }
+        basic.pause(duration)
+        stopRobot()
+        basic.clearScreen()
+    }
+
 }
