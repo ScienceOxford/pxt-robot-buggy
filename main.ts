06f0a2431f63ed30b48f1623e30cae6187ba2779
Sarah Townson <26084137+Isioviel@users.noreply.github.com>
2022-12-15 11:05:28 +0000

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,1 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Heart)
-    robotBuggy.movement(Dir.forward, Speed.medium, 2000)
-    basic.clearScreen()
-})
-input.onButtonPressed(Button.B, function () {
-    robotBuggy.movementIndependent(
-    750,
-    750,
-    2000,
-    IconNames.Heart
-    )
-})
 robotBuggy.activateRobot()
